eeadad74044747ea0429e11a3d8231b73eb12c4d
Agustin notebook <ronedev25@gmail.com>
2022-07-24 23:32:48 -0300

    add: partial for card in home. feat: ads of houses and apartments for sale in home view

--- controllers/appController.js
+++ controllers/appController.js
@@ -2,15 +2,35 @@ import { Precio, Categoria, Propiedad } from '../models/index.js'
 
 const home = async (req, res)=>{
 
-    const [categorias, precios] = await Promise.all([
+    const [categorias, precios, casas, departamentos] = await Promise.all([
         Categoria.findAll({raw: true}),
-        Precio.findAll({raw: true})
+        Precio.findAll({raw: true}),
+        Propiedad.findAll({
+            limit: 3,
+            where:{ categoryId: 1 },
+            include: [
+                {model: Precio, as: 'precio'},
+                {model: Categoria, as: 'categoria'},
+            ],
+            order: [['createdAt', 'DESC']]
+        }),
+        Propiedad.findAll({
+            limit: 3,
+            where:{ categoryId: 2 },
+            include: [
+                {model: Precio, as: 'precio'},
+                {model: Categoria, as: 'categoria'},
+            ],
+            order: [['createdAt', 'DESC']]
+        })
     ])
 
     res.render('home', {
         page: 'Home',
         categorys: categorias,
-        prices: precios
+        prices: precios,
+        homes: casas,
+        dptos: departamentos
     })
 }
 const category = (req, res)=>{

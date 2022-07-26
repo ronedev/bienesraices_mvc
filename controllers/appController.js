422d129d13c0d9871ee5ecfcb77826a41c9aab5e
Agustin notebook <ronedev25@gmail.com>
2022-07-26 01:30:15 -0300

    add: views and controllers of categories and 404 page

--- controllers/appController.js
+++ controllers/appController.js
@@ -1,49 +1,67 @@
-import { Precio, Categoria, Propiedad } from '../models/index.js'
+import { Precio, Categoria, Propiedad } from "../models/index.js";
 
-const home = async (req, res)=>{
+const home = async (req, res) => {
+  const [categorias, precios, casas, departamentos] = await Promise.all([
+    Categoria.findAll({ raw: true }),
+    Precio.findAll({ raw: true }),
+    Propiedad.findAll({
+      limit: 3,
+      where: { categoryId: 1 },
+      include: [
+        { model: Precio, as: "precio" },
+        { model: Categoria, as: "categoria" },
+      ],
+      order: [["createdAt", "DESC"]],
+    }),
+    Propiedad.findAll({
+      limit: 3,
+      where: { categoryId: 2 },
+      include: [
+        { model: Precio, as: "precio" },
+        { model: Categoria, as: "categoria" },
+      ],
+      order: [["createdAt", "DESC"]],
+    }),
+  ]);
 
-    const [categorias, precios, casas, departamentos] = await Promise.all([
-        Categoria.findAll({raw: true}),
-        Precio.findAll({raw: true}),
-        Propiedad.findAll({
-            limit: 3,
-            where:{ categoryId: 1 },
-            include: [
-                {model: Precio, as: 'precio'},
-                {model: Categoria, as: 'categoria'},
-            ],
-            order: [['createdAt', 'DESC']]
-        }),
-        Propiedad.findAll({
-            limit: 3,
-            where:{ categoryId: 2 },
-            include: [
-                {model: Precio, as: 'precio'},
-                {model: Categoria, as: 'categoria'},
-            ],
-            order: [['createdAt', 'DESC']]
-        })
-    ])
+  res.render("home", {
+    page: "Home",
+    categorys: categorias,
+    prices: precios,
+    homes: casas,
+    dptos: departamentos,
+  });
+};
+const category = async (req, res) => {
+  const { id } = req.params;
 
-    res.render('home', {
-        page: 'Home',
-        categorys: categorias,
-        prices: precios,
-        homes: casas,
-        dptos: departamentos
-    })
-}
-const category = (req, res)=>{
+  //Comprobar que la categoria que visitamos exista
+  const categoria = await Categoria.findByPk(id);
+  if (!categoria) {
+    return res.redirect("/404");
+  }
 
-}
-const noFounded = (req, res)=>{
+  //Obtener las propiedades de esa categoria
+  const propiedades = await Propiedad.findAll({
+    where: { categoryId: id },
+    include: [
+      { model: Categoria, as: "categoria" },
+      { model: Precio, as: "precio" },
+    ],
+  });
 
-}
+  res.render("category", {
+    page: `${categoria.name}s en venta`,
+    propiedades,
+  });
+};
 
-const searcher = (req, res)=>{
+const noFounded = (req, res) => {
+  res.render("404", {
+    page: "No encontrada",
+  });
+};
 
-}
+const searcher = (req, res) => {};
 
-export{
-    home, category, noFounded, searcher
-}
+export { home, category, noFounded, searcher };
